71d5991037a1f3dc7c9ba9b51ce9f8526376bfb0
Sandip Gill <singhgillsandip@gmail.com>
2023-04-29 22:15:46 +0530

    Add comment

--- src/main/java/com/xdecoders/voiceChatGpt/API.kt
+++ src/main/java/com/xdecoders/voiceChatGpt/API.kt
@@ -1,4 +1,4 @@
 package com.xdecoders.voiceChatGpt
 
 const val API_URL = "https://api.openai.com/v1/completions"
-const val API = "XXXXX-XXXXX-XXXXX-XXXXXX"
+const val API = "XXXXX-XXXXX-XXXXX-XXXXXX" // Add your openAPI Key
